4a8fd165f032cc35bf83a6efcec88d517c11257d
Luisrc07 <luisrodrgzc@gmail.com>
2025-10-20 16:21:07 -0400

    org

--- InvWEB/InvWEBAPP/urls.py
+++ InvWEB/InvWEBAPP/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from InvWEBAPP import views
 
 urlpatterns = [
-    path('', views.prueba, name= 'prueba')
+    path('', views.prueba, name= 'Home')
    
 ]
--- InvWEB/InvWEBAPP/views.py
+++ InvWEB/InvWEBAPP/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def prueba(request):
-    return render(request, "InvWEBAPP/prueba.html")
+    return render(request, "InvWEBAPP/home.html")
