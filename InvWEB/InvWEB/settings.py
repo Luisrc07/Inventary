f5cfa4cd456a947d3256f7c5b1d10b80b1028027
luipi <luisrodrgzc@gmail.com>
2025-11-13 08:51:14 -0400

    actualizacion de licencia y mas nancy

--- InvWEB/InvWEB/settings.py
+++ InvWEB/InvWEB/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'InvWEB.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'invweb_db',
-        'USER': 'invweb_user',
+        'NAME': 'inventario_db',
+        'USER': 'inventario_User',
         'PASSWORD': 'inventary12345',
         'HOST': 'localhost',
         'PORT': '5432',
